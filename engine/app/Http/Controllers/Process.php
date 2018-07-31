cee554ec1fef6f4427e17d70862751c2834342d5
Marcelo Silva <msilva@arkhotech.com>
2018-07-30 22:59:29 -0400

    Inicializacion de Laravel

---
+++ engine/app/Http/Controllers/Process.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Process extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+    
+    public function listProcess(){
+        return response()->json(array("status" => "ok"));
+    }
+}
