9c4b5228d979207a5e3d96a125dab596ed656feb
Marcelo Silva <msilva@arkhotech.com>
2018-08-16 00:32:29 -0300

    Seguridad y asignación de roles

--- engine/database/migrations/2018_08_16_011854_roles_properties.php
+++ engine/database/migrations/2018_08_16_011854_roles_properties.php
@@ -14,6 +14,33 @@ class RolesProperties extends Migration
     public function up()
     {
         //
+        Schema::create('process_roles',function(Blueprint $table){
+            $table->unsignedInteger('process_id');
+            $table->unsignedInteger('roles_id');
+//            $table->foreign('roles_id')
+//                    ->references('roles')
+//                    ->on('id')
+//                    ->onDelete('cascade');
+//            $table->foreign('process_id')
+//                    ->references('processes')
+//                    ->on('id')
+//                    ->onDelete('cascade');
+        });
+        
+        Schema::create('activity_roles',function(Blueprint $table){
+            $table->unsignedInteger('process_id');
+            $table->unsignedInteger('roles_id');
+//            $table->foreign('roles_id')
+//                    ->references('roles')
+//                    ->on('id')
+//                    ->onDelete('cascade');
+//            $table->foreign('process_id')
+//                    ->references('processes')
+//                    ->on('id')
+//                    ->onDelete('cascade');
+        });
+        
+        
     }
 
     /**
@@ -24,5 +51,8 @@ class RolesProperties extends Migration
     public function down()
     {
         //
+        Schema::dropIfExists('process_roles');
+        Schema::dropIfExists('activity_roles');
+        
     }
 }
